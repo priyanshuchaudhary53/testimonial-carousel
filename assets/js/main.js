8effbeec456d4b183eb1fb0e09988b880e7ed782
Priyanshu Chaudhary <85029348+priyanshuchaudhary53@users.noreply.github.com>
2021-08-08 03:36:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,16 @@
 var swiper_ts = new Swiper( '.ts_swiper_container', {
-    // Optional parameters
+
     loop: true,
     hashNavigation: true,
     spaceBetween: 50,
     autoHeight: true,
     speed: 750,
 
-    // If we need pagination
     pagination: {
         el: '.ts_swiper_pagination',
         clickable: true,
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.ts_swiper_button_next',
         prevEl: '.ts_swiper_button_prev',
